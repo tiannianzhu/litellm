372a384c1b5f5731f97489ed2f61ed1a440e4b62
zhutiannian <96524963+tiannianzhu@users.noreply.github.com>
2026-07-12 19:47:28 +0800

    feat(search): add Bocha search provider

--- litellm/utils.py
+++ litellm/utils.py
@@ -9796,6 +9796,7 @@ class ProviderConfigManager:
         )
         from litellm.llms.azure.search.transformation import BingGroundingSearchConfig
         from litellm.llms.bedrock.search.transformation import AgentCoreSearchConfig
+        from litellm.llms.bocha.search.transformation import BochaSearchConfig
         from litellm.llms.brave.search.transformation import BraveSearchConfig
         from litellm.llms.dataforseo.search.transformation import DataForSEOSearchConfig
         from litellm.llms.duckduckgo.search.transformation import DuckDuckGoSearchConfig
@@ -9837,6 +9838,7 @@ class ProviderConfigManager:
             SearchProviders.AGENTCORE: AgentCoreSearchConfig,
             SearchProviders.NIMBLE: NimbleSearchConfig,
             SearchProviders.BING_GROUNDING: BingGroundingSearchConfig,
+            SearchProviders.BOCHA: BochaSearchConfig,
         }
         config_class: Final = PROVIDER_TO_CONFIG_MAP.get(provider, None)
         if config_class is None:
